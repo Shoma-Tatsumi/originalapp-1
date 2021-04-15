31f8f6e7b73dddc729c42d0bebabba8fe8d7118b
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-15 19:03:56 +0900

    Set strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def production?
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :years_of_experience_id, :introduction])
   end
 end
