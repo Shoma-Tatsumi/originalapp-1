bc745aa5a39ca83c40f306d8d8e7ff5f42dd2680
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-15 19:07:42 +0900

    Run user model's test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers.' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :years_of_experience
+
 end
