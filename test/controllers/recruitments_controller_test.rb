e0e6ced793a721d5d9f82f52c0693fbe830a2a67
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-17 15:23:17 +0900

    Run rubocop

--- test/controllers/recruitments_controller_test.rb
+++ test/controllers/recruitments_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class RecruitmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get recruitments_index_url
     assert_response :success
   end
-
 end
