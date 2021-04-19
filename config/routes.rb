60a3d48dd4b94db797eb8752926768f1a6299b64
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-19 15:09:23 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-    }
+  devise_for :users
   root 'prefectures#index'
   resources :prefectures, only: :index do
     resources :recruitments, only: :index
   end
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 end
