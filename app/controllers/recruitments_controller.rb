e46404d51340eef424bd1495fbccc87c1961ddc6
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-04-20 18:46:09 +0900

    Create new recruiting screen

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -2,7 +2,25 @@
 
 class RecruitmentsController < ApplicationController
   def index
+    @prefecture_id = params[:prefecture_id].to_i
+    @recruitments = Recruitment.all.order('created_at DESC')
   end
+
   def new
+    @recruitment = Recruitment.new
+  end
+
+  def create
+    @recruitment = Recruitment.create(recruitment_params)
+    if @recruitment.save
+      redirect_to prefecture_recruitments_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def recruitment_params
+    params.require(:recruitment).permit(:event_date, :start_time, :end_time, :place, :number_of_people, :category_id, :description).merge(user_id: current_user.id, prefecture_id: params[:prefecture_id])
   end
 end
