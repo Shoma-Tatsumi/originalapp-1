857f85e5bfbc1d2ecf589bf52feb9750165314ca
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-05-01 23:13:16 +0900

    Create comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'comments/create'
   devise_for :users
   root 'prefectures#index'
   resources :prefectures, only: :index do
