45323d6f6ee1856e8c1571767e0c4c32c48094b1
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-05-04 17:51:48 +0900

    Set to prevent page transitions when not signed in

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RecruitmentsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_recruitment, only: %i[show edit update destroy]
 
   def index
