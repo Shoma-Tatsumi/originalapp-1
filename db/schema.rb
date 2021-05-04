5fc65ad27f39b651542adac9cfd7b56c5611588c
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-05-04 16:02:02 +0900

    Update comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_072842) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "recruitment_id"
-    t.text "text"
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
