6611eed30d30681637819505ebd5d8a5fb0fd93f
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-05-15 15:33:25 +0900

    Updated Japanese localization settings

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -16,19 +16,19 @@ class Recruitment < ApplicationRecord
     validates :start_time
     validates :end_time
     validates :place
-    validates :number_of_people, numericality: { only_integer: true, message: 'is only half-width number' }
+    validates :number_of_people, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
     validates :description
     validates :prefecture_id
   end
-  validates :category_id, numericality: { other_than: 0, message: 'Select' }
+  validates :category_id, numericality: { other_than: 0, message: 'を選択してください' }
 
   def event_date_cannot_be_in_the_past
-    errors.add(:event_date, "can't select a date in the past") if event_date.present? && event_date < Date.today
+    errors.add(:event_date, "は過去の日付を選択できません") if event_date.present? && event_date < Date.today
   end
 
   def start_end_time
     if start_time.present? && end_time.present? && start_time > end_time
-      errors.add(:end_time, "can't select a time later than the start time")
+      errors.add(:end_time, "は開始時刻より遅く選択して下さい")
     end
   end
 end
