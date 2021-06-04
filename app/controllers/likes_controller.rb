5f80515fa3306e9e99c2643d7ff18d407d20d3af
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-06-04 17:28:48 +0900

    Set controller_action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.create(recruitment_id: params[:recruitment_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @like = Like.find_by(recruitment_id: params[recruitment_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
